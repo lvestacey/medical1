df36c21c4bdc3500d445ffc37edd5c3ebaa9c047
lvestacey <monizkr@gmail.com>
2024-04-26 18:44:34 +0900

    0426 파이썬수업

--- w0425/w0425_02.py
+++ w0425/w0425_02.py
@@ -1,17 +1,40 @@
-import requests
-from bs4 import BeautifulSoup
 import time
-import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36","Accept-Language":"ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7"}
+URL = "https://flight.naver.com/"
 
-url="https://www.naver.com"
 
-# 브라우저 열기 
-browser=webdriver.Chrome()
-browser.maximize_window() # 창 최대화
-browser.get(url)
+def compute_date(dates, target) -> list:
+    print("compute entire date information from current month to limit")
+    return [date for date in dates if date.text == str(target)]
 
+
+browser = webdriver.Chrome()
+# browser.maximize_window()
+
+browser.get(URL)
+time.sleep(1)
+
+# remove AD popup
+find = browser.find_elements(By.CLASS_NAME, "anchor")
+for f in find:
+    if f.get_attribute("title") == "지금 바로 혜택 확인하기":
+        browser.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[9]/div/div[2]/button[1]').click()
+        print("remove pop up")
+        break
+
+# push 가는 날 button also delay 1 sec is necessary, if don't use this, can not read date information.
+browser.find_element(By.XPATH, '//*[@id="__next"]/div/div[1]/div[4]/div/div/div[2]/div[2]/button[1]').click()
+time.sleep(1)
+
+# read date information and push 25 day
+dates = browser.find_elements(By.CLASS_NAME, "sc-evZas dDVwEk num".replace(" ", "."))
+compute_date(dates, 25)[0].click()
+# read date information and push 30 day
+dates = browser.find_elements(By.CLASS_NAME, "sc-evZas dDVwEk num".replace(" ", "."))
+compute_date(dates, 30)[0].click()
+
+
+while True:
+    pass
